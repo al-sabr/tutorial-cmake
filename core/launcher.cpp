45886b5d1beffd72510df5f8f6e7c1770802d8be
ergü <ergu@erguder.tk>
2019-03-30 03:13:25 +0100

    Debugging added that display which program is executed.

--- core/launcher.cpp
+++ core/launcher.cpp
@@ -73,7 +73,7 @@ int main(int argc, char* argv[]){
     }else{
         std::printf( "DLL folder removing  failed (%d).\n", GetLastError());
     }
-
+    
     return 0;
 }
 
--- core/main.cpp
+++ core/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <module1.h>
+#include <string>
 
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
     cout << endl;
+    string filepath(argv[0]);
+    cout << "filepath = " << filepath << endl;
     cout << "Main function" << endl;
     module1::test(155);
+    cout << std::flush;
+    return 0;
 }
